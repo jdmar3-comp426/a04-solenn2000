3cceef70a678ff23d5218717ec03b19210065c28
solenn2000 <46977213+solenn2000@users.noreply.github.com>
2021-11-16 10:40:49 -0500

    update

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.get("/app/users", (req, res) => {
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {
 	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
-	//res.status(200).json({"id":req.params.id,"user":stmt});
-	console.log({"id":req.params.id,"user":stmt})
+	res.status(200).json({"id":req.params.id,"user":stmt});
+	//console.log({"id":req.params.id,"user":stmt})
 
 });
 // UPDATE A SINGLE USER (HTTP method PATCH) at endpoint /app/update/user/:id
